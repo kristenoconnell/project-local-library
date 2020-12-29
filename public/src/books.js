d74dc980baf59a70cc6a86da882bce16ef0724c5
Kristen O'Connell <75442051+kristenoconnell@users.noreply.github.com>
2020-12-28 22:39:43 -0600

    books.js KO
    Project submission

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,26 @@
-function findAuthorById(authors, id) {}
+const findAuthorById = (authors, id) => authors.find((author) => author.id === id)
 
-function findBookById(books, id) {}
+const findBookById = (books, id) => books.find((book) => book.id === id)
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const partitioned = books.reduce((acc, currentBook) => {
+    const book = currentBook
+    const borrowed = book.borrows
+      !borrowed[0].returned ? acc[0].push(book) : acc[1].push(book)
+      return acc
+  }, [[], []])
+  return partitioned
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  const borrowed = book.borrows
+  const borrowArray = borrowed.map(record => {
+      const foundAccount = accounts.find((account) => account.id === record.id)
+      const newRecord = { ...record, ...foundAccount }
+      return newRecord
+  })
+  return borrowArray.slice(0,10)
+}
 
 module.exports = {
   findAuthorById,
