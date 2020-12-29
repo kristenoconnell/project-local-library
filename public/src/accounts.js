84349859d7e6c9c651c052a467ef1e5b944e7ee7
Kristen O'Connell <75442051+kristenoconnell@users.noreply.github.com>
2020-12-28 22:35:25 -0600

    accounts.js KO
    Project submission

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,41 @@
-function findAccountById(accounts, id) {}
+const findAccountById = (accounts, id) => accounts.find((account) =>
+  account.id === id)
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  const mapped = accounts.map(account => {
+      let result = {}
+      const { name : { first, last } } = account
+      result = { name: { first, last } }
+    return result
+    })
+  const sorted = mapped.sort((name1, name2) => 
+      name1.name["last"].toLowerCase() > name2.name["last"].toLowerCase() ? 1 : -1)
+  return sorted
+}
 
-function numberOfBorrows(account, books) {}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function numberOfBorrows(account, books) {
+  const mapped = books.map((book) => book.borrows)
+  let result = 0
+  for (let currentBorrows of mapped) {
+      for (let currentAccount in currentBorrows) {
+        if (currentBorrows[currentAccount].id === account.id)
+        result++
+      }
+    }
+  return result
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  const filtered = books.filter(book => {
+    if (book.borrows[0].id === account.id && !book.borrows.returned) {
+      const author = authors.find((author) => author.id === book.authorId)
+        book.author = author
+        return book 
+      }
+   })
+  return filtered 
+}
 
 module.exports = {
   findAccountById,
